786e2d197ca96b2622fc9e500d033440087c0f24
vascsx <vasczn@gmail.com>
2025-04-04 23:17:32 -0300

    estrutura inicial do projeto com Robot framework

--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -1,12 +1,46 @@
 //describe a suite dos testes, que endpoint iremos testar ou metódo....
 describe(' RegisterAPI - Endpoint Register', () => {
-
+  
   // it - cenários
-  it.only('Cenário em que o usuário já esteja cadastrado', () => {
+it('Cenário em que o usuário já esteja cadastrado', () => {
+
+    //body
+    const newUser = {
+      "fullName": "Thais teste",
+      "email": "teste123@gmail.com",
+      "password": "Tha#2344"
+    }
+
+    //Envio da requisição
     cy.request({
       method: 'POST',
       url: Cypress.env('apiRegister'),
-      // failOnStatusCode: false,
+      body: newUser,
+      failOnStatusCode: false,
+    }).should(({ status, body }) => {
+      expect(status).to.equal(400);
+      expect(body.erros).to.include("E-mail já cadastrado.");
     });
-  })
+})
+
+it('Cenário em que o usuário já esteja cadastrado', () => {
+
+  //body
+  const newUser = {
+    "fullName": "",
+    "email": "teste121@gmail.com",
+    "password": "Tha#2344"
+  }
+
+  //Envio da requisição
+  cy.request({
+    method: 'POST'/,
+    url: Cypress.env('apiRegister'),
+    body: newUser,
+    failOnStatusCode: false,
+  }).should(({ status, body }) => {
+    expect(status).to.equal(400);
+    expect(body.erros).to.include("O campo 'FullName' é obrigatório.");
+  });
+})
 })
